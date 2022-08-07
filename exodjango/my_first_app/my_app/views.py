084f0542e4657ca2c05ae219ea107ee90488ac58
Jérémy Vangansberg <jeremy.vangansberg@gmail.com>
2022-08-07 11:26:47 +0200

    55

--- exodjango/my_first_app/my_app/views.py
+++ exodjango/my_first_app/my_app/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.http.response import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 articles = {
@@ -9,6 +11,21 @@ articles = {
     'mariage': 'Camille Vangansberg ?'
 }
 
-
 def news_view(request, topic):
-    return HttpResponse(articles[topic])
+    try :
+        result = articles[topic]
+        return HttpResponse(result)
+    except :
+        # result = 'The topic doesnt exist'
+        # return HttpResponseNotFound(result)
+        raise Http404('GENERIC ERROR') # 404.html
+
+# def add_view(request, num1, num2):
+#     result = num1 + num2
+#     return HttpResponse(str(result))
+
+def num_page_view(request,num_page):
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+    print(topic)
+    return HttpResponseRedirect(reverse('topic-page', args=[topic]))
