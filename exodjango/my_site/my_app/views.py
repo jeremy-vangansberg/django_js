1c09c3a8ef45e68cb0465d4f7a9e7d13b2aaf3bc
Jérémy Vangansberg <jeremy.vangansberg@gmail.com>
2022-08-31 11:35:26 +0200

    django templates

--- exodjango/my_site/my_app/views.py
+++ exodjango/my_site/my_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def example_view(request):
+    # my_app/templates/my_app
+    return render(request, 'my_app/example.html')
+
+
+def variable_view(request):
+
+    my_var = {'first_name':'Rosaline', 'last_name':'Franklin',
+     'some_list': [1,2,3,4,5,6], 'some_dict':{'my_key': 'my_value'}}
+
+    return render(request, 'my_app/variables.html', context=my_var)
